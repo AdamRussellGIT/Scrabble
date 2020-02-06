481f52b7075e6301dc670590639f8bc0516e137a
karolucd <karolwojcik29@gmail.com>
2020-02-06 11:58:45 +0000

    Includes resetBoard Method Check basic layout

--- Scrabble/src/Board.java
+++ Scrabble/src/Board.java
@@ -12,6 +12,31 @@ public class Board
 		
 	}
 	
+	public Tile[][] resetBoard()
+	{
+		for(int i = 0;i < board.length;i++)
+		{
+			for(int j = 0;j < board.length;j++)
+			{
+				board[i][j] = null;
+			}
+		}
+		return board;
+	}
+	
+	
+	public void wordPlacementCheck()
+	{
+		
+		
+		//Check if the tile is out of bounds
+		//Check if there is an existing tile already in the position
+		//if first word whether it's in the centre of the board
+		//if not first word if it's connected to another word on the board
+		//if the player has the necessary letters
+		//if the word conflicts with existing letters
+		//if placement uses at least one letter from rack
+	}
 	public String toString()
 	{
 		String gameBoard = "";
