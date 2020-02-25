3db06c0f1ca2be78e9bbda9950946e7c05be10e8
karolucd <karolwojcik29@gmail.com>
2020-02-25 15:02:11 +0000

    gridPane board

--- Scrabble/src/UI.java
+++ Scrabble/src/UI.java
@@ -11,15 +11,30 @@
 //    Displays a graphical representation of the board
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import javafx.scene.image.Image;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UI extends Application
 {
+    Stage curr_window;
+    Button my_button;
+
     public void print(String s)
     {
         System.out.println(s);
@@ -44,17 +59,66 @@ public class UI extends Application
         }
     }
 
-    Stage curr_window;
+
+
+
+
+
+    public void displayBoard()
+    {
+
+
+    }
 
 
     @Override
     public void start(Stage Scrabble)
     {
+        //Images for background
+//        Image board = new Image(new FileInputStream("C:\\Users\\karol\\IdeaProjects\\Camel-bois\\Scrabble\\res\\scrabbleBoard.png"));
+//        Image frame = new Image(new FileInputStream("C:\\Users\\karol\\IdeaProjects\\Camel-bois\\Scrabble\\res\\scrabbleFrame.png"));
+//
+//        ImageView imageView = new ImageView(board);
+//        ImageView imageView2 = new ImageView(frame);
+//
+//
+//        imageView.setY(50);
+//        imageView.setX(20);
+//
+//        imageView2.setY(520);
+//        imageView2.setX(25);
+//
+//        imageView.setFitHeight(455);
+//        imageView.setFitWidth(500);
+//
+//        imageView2.setFitHeight(760);
+//        imageView2.setFitWidth(200);
+//
+//        imageView.setPreserveRatio(true);
+//        imageView2.setPreserveRatio(true);
+//
+//        Group root = new Group(imageView,imageView2);
+
+        GridPane gridPane = new GridPane();
+
         curr_window = Scrabble;
         curr_window.setTitle("Scrabble");
-        StackPane the_layout = new StackPane();
 
-        Scene scene = new Scene(the_layout, 500, 400);
+        gridPane.setHgap(7);
+        gridPane.setVgap(7);
+
+
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                Button butt = new Button("(" + i + "," + j + ")");
+                butt.setPrefSize(55,55);
+                gridPane.add(butt, j, i, 1, 1);
+            }
+        }
+
+        Scene scene = new Scene(gridPane, 900, 800);
 
         Scrabble.setScene(scene);
         Scrabble.show();
