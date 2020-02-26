3d91a44e4b45832929d7bbf3e42e5f1243310b54
AdamRusselGIT <adam.russell@ucdconnect.ie>
2020-02-26 13:18:56 +0000

    Word class first implementation

---
+++ Scrabble/src/Word.java
@@ -0,0 +1,45 @@
+public class Word {
+    private int startRow;
+    private int startColumn;
+    private char direction;
+    private String word;
+
+    public Word(int startRow, int startColumn, char direction, String word) {
+        this.startRow = startRow;
+        this.startColumn = startColumn;
+        this.direction = direction;
+        this.word = word;
+    }
+
+    public int getStartRow() {
+        return this.startRow;
+    }
+
+    public int getStartColumn() {
+        return this.startColumn;
+    }
+
+    public char getDirection() {
+        return this.direction;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public void setStartRow(int startRow) {
+        this.startRow = startRow;
+    }
+
+    public void setStartColumn(int startColumn) {
+        this.startColumn = startColumn;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
