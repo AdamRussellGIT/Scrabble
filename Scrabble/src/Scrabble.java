96dba9c3ec0b83e2b053d0fa8d87e0d5e9658247
AdamRusselGIT <adam.russell@ucdconnect.ie>
2020-03-06 08:55:07 +0000

    Edited Scrabble class to remove legacy functionality and clean it up

--- Scrabble/src/Scrabble.java
+++ Scrabble/src/Scrabble.java
@@ -11,106 +11,7 @@ import java.util.Scanner;
 
 public class Scrabble
 {
-    Player player1;
-    Player player2;
-    Pool gamePool;
-    Board gameBoard;
-    UI gameUI;
-
-    int turn = 0;
-    Player currentPlayer;
-    String choice;
-
-    int previousScore = 0;
-    Board previousBoard = new Board();
-
-    /*public Scrabble()
-    {
-        gamePool = new Pool();
-        gameBoard = new Board();
-        gameUI = new UI();
-    }*/
-
-    /*public void playGame()
-    {
-        gameUI.print("Enter the name for Player 1 : ");
-        String name1 = gameUI.getInput();
-        gameUI.print("Enter the name for Player 2 : ");
-        String name2 = gameUI.getInput();
-        gameUI.print("Let's play!");
-
-        player1 = new Player(name1, gamePool);
-        player2 = new Player(name2, gamePool);
-
-        while (!gamePool.poolEmpty() && (!player1.frame.checkEmptyFrame() || !player2.frame.checkEmptyFrame()))
-        {
-            if (turn % 2 == 0) {
-                currentPlayer = player1;
-            } else {
-                currentPlayer = player2;
-            }
-
-            //set a previous board incase we need to remove letters after a players turn
-            for (int i = 0; i < 15; i++)
-            {
-                for (int j = 0; j < 15; j++)
-                {
-                    for (int k = 0; k < 2; k++)
-                    {
-                        previousBoard.board[i][j][k] = gameBoard.board[i][j][k];
-                    }
-                }
-            }
-
-            System.out.println("Current player: " + currentPlayer.getName() + " and their score : " + currentPlayer.getScore());
-            System.out.println(currentPlayer.toString());
-
-            do {
-                gameUI.print("Enter 'QUIT', 'PASS', 'EXCHANGE', 'HELP', 'PLACEWORD'");
-                choice = gameUI.getInput();
-            } while (!gameUI.checkChoice(choice));
-
-            //don't increment turn if the player wants help
-            if (choice.equals("HELP")) {
-                help();
-            }
-
-            else if (choice.equals("PLACEWORD")) {
-                //if they placed a valid word, move to the next turn
-                if (placeWordBoard(currentPlayer))
-                {
-                    turn++;
-                }
-
-                //if they did not pass the wordPlacementCheck, stay on the current players turn and tell them
-                else
-                {
-                    gameUI.print("That was an invalid word placement!");
-                }
-            }
-
-            else {
-                if (choice.equals("QUIT")) {
-                    gameUI.print("User chose to quit the game!");
-                    gamePool.workPool.clear();
-                }
-
-                if (choice.equals("PASS")) {
-                    gameUI.print("User chose to pass!");
-                }
-
-                if (choice.equals("EXCHANGE")) {
-                    exchange(gamePool, currentPlayer);
-                }
-
-                turn++;
-            }
-            currentPlayer.frame.refillFrame(gamePool);
-            System.out.println(gameBoard.toString());
-        }
-    }
-
-    public ArrayList<Word> findAllWords(int row, int col, char dir, String wrd){
+    public ArrayList<Word> findAllWords(int row, int col, char dir, String wrd, Board gameBoard, Board previousBoard){
         ArrayList<Word> ans = new ArrayList<Word>();
         ans.add(new Word(row,col,dir,wrd));
         if(dir=='A' || dir=='a'){
@@ -166,7 +67,7 @@ public class Scrabble
 
         }
         return ans;
-    }*/
+    }
 
 
     public boolean exchange(Pool gamePool, Player currentPlayer, String[] parsedInput)
@@ -201,46 +102,9 @@ public class Scrabble
 
         return true;
     }
-    /*
-    public boolean placeWordBoard(Player currentPlayer)
+
+    public void calculateScore(ArrayList<Word> wordsArray, Player currentPlayer, Board gameBoard, int previousScore)
     {
-        int row;
-        int column;
-        char direction;
-        String word;
-
-        gameUI.print("Enter row that your word will start on : ");
-        row = Integer.parseInt(gameUI.getInput());
-
-        gameUI.print("Enter column that your word will start on : ");
-        column = Integer.parseInt(gameUI.getInput());
-
-        gameUI.print("Enter direction your word will go : ");
-        direction = gameUI.getInput().charAt(0);
-
-        gameUI.print("Enter the word you want to place");
-        word = gameUI.getInput();
-
-        if(gameBoard.wordPlacementCheck(row, column, direction, word, currentPlayer))
-        {
-            gameBoard.placeWord(row, column, direction, word, currentPlayer);
-            calculateScore(findAllWords(row, column, direction, word), currentPlayer);
-            return true;
-        }
-
-        else
-        {
-            return false;
-        }
-    }
-
-    public void calculateScore(ArrayList<Word> wordsArray, Player currentPlayer)
-    {
-        System.out.println("Hello there");
-        for (int i = 0; i < wordsArray.size(); i++)
-        {
-            System.out.println(wordsArray.get(i).getWord());
-        }
         int score = 0;
         int wordMultiplier;
         int letterScore;
@@ -350,16 +214,4 @@ public class Scrabble
         previousScore = score;
         currentPlayer.setScore(score);
     }
-
-    public void help()
-    {
-        System.out.println("QUIT: Quits the game and exits.");
-        System.out.println("PASS: Passes the current players turn, effectively doing nothing.");
-        System.out.println("EXCHANGE: Allows you to exchange some or all of your tiles.");
-        System.out.println("PLACEWORD: Enter the row your word starts with/nEnter the column where your word starts.");
-        System.out.println("/tEnter the direction your word goes (A, a, D, d, are accepted), press enter");
-        System.out.println("/tEnter the word you want placed");
-        System.out.println("NOTE: If there is a 'G' on the board, and you wish to place the word GET, and you only have E and T in your frame,");
-        System.out.println("you must specify that the word starts in the row and column where G is, and say that the word you want to place is GET");
-    }*/
 }
