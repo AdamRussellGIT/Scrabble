f281e104770aa18df0784fd66145f1ade0a6cca8
AdamRusselGIT <adam.russell@ucdconnect.ie>
2020-04-08 08:59:33 +0100

    Added PlayerAPI to bot branch

---
+++ Scrabble/src/PlayerAPI.java
@@ -0,0 +1,12 @@
+public interface PlayerAPI
+{
+     int getPrintableId();
+
+     public String getName();
+
+     public int getScore();
+
+     public String getFrameAsString();
+
+     public String toString();
+}
